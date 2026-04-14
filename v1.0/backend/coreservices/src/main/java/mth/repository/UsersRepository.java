c92c9dd0b44fcdcaf483462cd64f65bd2ad1adac
drkpn5848 <drkpn.in@gmail.com>
2026-04-14 09:49:20 +0530

    v1

---
+++ v1.0/backend/coreservices/src/main/java/mth/repository/UsersRepository.java
@@ -0,0 +1,19 @@
+package mth.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import mth.models.Users;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+	
+	@Query("select U.role from Users U where U.email=:username and U.password=:password")
+	public Object validateCredentials(@Param("username") String username, @Param("password") String password);
+	
+	@Query("select U.id from Users U where U.email=:email")
+	public Object checkByEmail(@Param("email") String email);
+	
+}
